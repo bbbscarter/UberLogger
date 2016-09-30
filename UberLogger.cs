bb41cf14caec06fd34c39f74a0fdfb11e140d19b
Steve Streeting <steve@stevestreeting.com>
2016-09-30 10:57:38 +0100

    Fix warnings from unused packages

--- UberLogger.cs
+++ UberLogger.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System;
--- UberLoggerFile.cs
+++ UberLoggerFile.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UberLogger;
 using System.IO;
 using UnityEngine;
